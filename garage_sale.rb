d0ffbae53a93b219bf75e2ebcba3ca55a321ff63
Randy Davis <randy_davis@outlook.com>
2014-06-25 23:12:19 -0500

    Finish all garage sale related exercises.

--- garage_sale.rb
+++ garage_sale.rb
@@ -2,5 +2,23 @@ garage_items =["computer", "couch", "black backpack", "rolling desk chair", "tab
 
 # garage_items.sort
 sorted_garage_items = garage_items.sort
-puts "sorted_garage_items"
+puts "sorted_garage_items:"
 puts sorted_garage_items
+puts "---------------------------------"
+
+# garage_items.reverse
+reversed_garage_items = garage_items.sort.reverse
+puts "reversed_garage_items:"
+puts reversed_garage_items
+puts "---------------------------------"
+
+# garage_items.capitalized
+garage_items_capitalized = garage_items.each {|item| item.capitalize!}
+puts "garage_items_capitalized:"
+puts garage_items_capitalized 
+puts "---------------------------------"
+
+puts "garage_items_ordered_list_capitalized:"
+garage_items_capitalized = garage_items.each_with_index do |value, index|
+		puts "#{index+1}. #{value}"
+	end
