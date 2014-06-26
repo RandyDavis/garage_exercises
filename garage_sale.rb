100f228aac5f35e523376af68c86fedd460a0d2c
Randy Davis <randy_davis@outlook.com>
2014-06-25 21:42:22 -0500

    Initial commit of garage_sale.rb practice on arrays

---
+++ garage_sale.rb
@@ -0,0 +1,6 @@
+garage_items =["computer", "couch", "black backpack", "rolling desk chair", "table", "blender"]
+
+# garage_items.sort
+sorted_garage_items = garage_items.sort
+puts "sorted_garage_items"
+puts sorted_garage_items
